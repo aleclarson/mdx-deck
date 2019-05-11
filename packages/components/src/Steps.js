349da1318a85d62a2be431fdca3d2d8ee4ecdf04
Alec Larson <alec.stanford.larson@gmail.com>
2019-05-11 11:53:51 -0400

    fix: reset notes on every step
    This ensures that the proper notes are shown when navigating backwards.
    
    It also allows for per-step notes.
    
    - add "items" prop to <Steps> component
    - wrap each item in a <Step> component
    - reset "meta.notes" on every slide/step change

--- packages/components/src/Steps.js
+++ packages/components/src/Steps.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import { useDeck } from './context'
 import useSteps from './useSteps'
+import { Step } from './useStep'
 
-export const Steps = props => {
-  const step = useSteps(props.length)
-  return props.render({ step })
+export const Steps = ({ items, length, render }) => {
+  const step = useSteps(items ? items.length : length)
+  if (items) {
+    return (
+      <div>
+        {items.map(
+          (item, i) =>
+            step >= i && (
+              <Step key={i} index={i}>
+                {typeof item == 'function' ? item(i) : item}
+              </Step>
+            )
+        )}
+      </div>
+    )
+  }
+  return render({ step })
 }
 
 export default Steps

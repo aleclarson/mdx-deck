bb100fd918810e1bab80b83a58157c4bd644e287
Alec Larson <alec.stanford.larson@gmail.com>
2019-05-11 13:41:53 -0400

    fixup: reset notes

--- packages/components/src/Steps.js
+++ packages/components/src/Steps.js
@@ -3,23 +3,28 @@ import { useDeck } from './context'
 import useSteps from './useSteps'
 import { Step } from './useStep'
 
-export const Steps = ({ items, length, render }) => {
-  const step = useSteps(items ? items.length : length)
-  if (items) {
-    return (
-      <div>
-        {items.map(
-          (item, i) =>
-            step >= i && (
-              <Step key={i} index={i}>
-                {typeof item == 'function' ? item(i) : item}
-              </Step>
-            )
-        )}
-      </div>
-    )
-  }
-  return render({ step })
+export const Steps = ({ items, render, ...props }) => {
+  const step = useSteps(items ? items.length : props.length)
+  return items ? (
+    <div>
+      {items.map((item, i) => {
+        const visible = props.split ? step === i : step >= i
+        return (
+          visible && (
+            <Step key={i} index={i}>
+              {render
+                ? render(item, i)
+                : typeof item === 'function'
+                ? item(i)
+                : item}
+            </Step>
+          )
+        )
+      })}
+    </div>
+  ) : (
+    render({ step })
+  )
 }
 
 export default Steps
